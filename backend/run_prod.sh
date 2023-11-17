77ce56fdcb93a1da10ff285b285f8a111e628b60
Denis Maslennikov <ktotom7@yandex.ru>
2023-11-17 18:28:32 +0300

    add prod configuration

---
+++ backend/run_prod.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+python /code/manage.py migrate --noinput
+
+python /code/manage.py collectstatic --noinput
+
+cp -r /static/django/. /static/
+
+gunicorn --config python:gunicorn_config server.wsgi
+
